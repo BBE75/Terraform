87be9cf495cd737928a6166ae55be5ecc0452370
BBE75 <beyerle.benjamin@gmail.com>
2020-05-31 18:44:00 +0200

    fixe logs

--- Part_1/diagLogs.tf
+++ Part_1/diagLogs.tf
@@ -1,6 +1,6 @@
 # Create storage account for boot diagnostics
 resource "azurerm_storage_account" "storageAccount1" {
-    name                        = "log-part1"
+    name                        = "logpart1"
     resource_group_name         = azurerm_resource_group.RG1.name
     location                    = azurerm_resource_group.RG1.location
     account_tier                = "Standard"
